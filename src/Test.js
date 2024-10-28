d1b107db8d80c8d2f029bc827858264a1e23db2c
Janak kr shrestha <janakkrshrestha@gmail.com>
2024-10-28 18:15:42 +0545

    (After) A Surprising Optimization Trick With children

--- src/Test.js
+++ src/Test.js
@@ -14,34 +14,34 @@ function SlowComponent() {
   );
 }
 
-// function Counter({ children }) {
-//   const [count, setCount] = useState(0);
-//   return (
-//     <div>
-//       <h1>Slow counter?!?</h1>
-//       <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
-//       {children}
-//     </div>
-//   );
-// }
-
-export default function Test() {
+function Counter({ children }) {
   const [count, setCount] = useState(0);
   return (
     <div>
       <h1>Slow counter?!?</h1>
       <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
-      <SlowComponent />
+      {children}
     </div>
   );
+}
 
+export default function Test() {
+  // const [count, setCount] = useState(0);
   // return (
   //   <div>
-  //     <h1>
-  //       <Counter>
-  //         <SlowComponent />
-  //       </Counter>
-  //     </h1>
+  //     <h1>Slow counter?!?</h1>
+  //     <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
+  //     <SlowComponent />
   //   </div>
   // );
+
+  return (
+    <div>
+      <h1>
+        <Counter>
+          <SlowComponent />
+        </Counter>
+      </h1>
+    </div>
+  );
 }
