40c6d65920c443052acc14e7355d7fb09908e5b4
Janak shrestha <janak0ff@gmail.com>
2024-11-25 10:20:31 +0545

    Creating a Redux Store

--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,23 +1,52 @@
-const initialState = {
-    balance: 0,
-    loan: 0,
-    loanPurpose: "",
+import { createStore } from "redux";
 
-}
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
 
 function reducer(state = initialState, action) {
-    switch (action.type) {
-        case "account/deposit":
-            return {...state, balance: state.balance + action.payload}
-        case "account/withdraw":
-            return {...state, balance: state.balance - action.payload}
-        case "account/requestLoan":
-            if (state.balance > 0) return state;
-        
-            return {...state, loan:  action.payload.purpose}
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
 
-            case "account/payLoan":
-                return {...state, balance: state.balance - state.loan, loan: 0, loanPurpose: ""}
-        default:
-            return state; 
-    }}
+    case "account/payLoan":
+      return {
+        ...state,
+        balance: state.balance - state.loan,
+        loan: 0,
+        loanPurpose: "",
+       
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer); // store
+
+store.dispatch({ type: "account/deposit", payload: 500 });
+store.dispatch({ type: "account/withdraw", payload: 200 });
+console.log(store.getState());
+
+
+store.dispatch({
+  type: "account/requestLoan",
+  payload: { amount: 1000, purpose: 'buy a car' },
+});
+
+console.log(store.getState());
+
+store.dispatch({ type: "account/payLoan" });
+console.log(store.getState());
