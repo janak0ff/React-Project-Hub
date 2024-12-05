c9d5ee854443c1114c60fa3f4f05349b4594312c
Janak shrestha <janak0ff@gmail.com>
2024-12-05 15:57:37 +0545

    Dispatching Actions from Our React App

--- redux-intro/src/features/customer/Customer.js
+++ redux-intro/src/features/customer/Customer.js
@@ -1,7 +1,7 @@
 import {useSelector} from 'react-redux';
 
 function Customer() {
-const customer = useSelector(store=>store.Customer.fullName)
+const customer = useSelector(store=>store.customer.fullName)
 // console.log(customer) 
 
 return <h2>👋 Welcome, {customer}</h2>;
