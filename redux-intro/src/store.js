9afe36ef7cfe7824fa49b8511f623884e19168e8
Janak shrestha <janak0ff@gmail.com>
2024-12-09 10:38:44 +0545

    Creating the Account Slice

--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,6 +1,3 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
 import { configureStore } from "@reduxjs/toolkit";
 
 import accountReducer from "./features/accounts/accountSlice";
